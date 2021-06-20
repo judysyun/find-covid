1da1b06f8e1090df4487825aaefbcd7b57f3b92d
Rebecca <rebecca.xie2@gmail.com>
2021-06-20 09:01:21 -0400

    updated

--- code.js
+++ code.js
@@ -2,16 +2,3 @@ function found(){
     document.getElementById("found-text").classList.remove("hide");
 }
 
-function startGame(){
-    document.getElementById("startScreen").style.display = "none";
-    startTimer();
-  };
-  
-function startTimer(){
-myTimer = setInterval(upTimer, 10);
-};
-
-function upTimer(){
-newTime = parseFloat(document.getElementById("timer").textContent) + 0.01;
-document.getElementById("timer").textContent = newTime.toFixed(3);
-};
